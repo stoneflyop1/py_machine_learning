aa2242967d67c24144af0f63326c4f10064f3c74
jeff <jefflyop1@gmail.com>
2017-05-31 21:11:07 +0800

    start chap02

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Machine Learning
+
+[Python Machine Learning Free e-Book](https://github.com/tahmid-choyon/free-tech-ebooks-from-packtpub/tree/master/Python%20Machine%20Learning)
---
+++ ch02/main02.py
@@ -0,0 +1,78 @@
+import pandas as pd
+
+df = pd.read_csv('./iris.data', header=None)
+# df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+# df.tail() # cannot show data when run in terminal
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# select data
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0,2]].values
+# show data
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+plt.xlabel('sepal length')
+plt.ylabel('petal length')
+plt.legend(loc='upper left')
+plt.show()
+
+# plot decision boundary
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, c1 in enumerate(np.unique(y)):
+        plt.scatter(x=X[y==c1, 0], y=X[y==c1, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=c1)
+
+# traing data
+from perceptron import Perceptron 
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+# plot error rates
+plt.plot(range(1, len(ppn.errors_) +1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
+plt.show()
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length (cm)')
+plt.ylabel('petal length (cm)')
+plt.legend(loc='upper left')
+plt.show()
+
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0]-X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1]-X[:,1].mean()) / X[:,1].std()
+
+from adaline import AdalineGD
+
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
