9d5f6ce86e76e5568693a390d4a27e4601ce2848
jeff <jefflyop1@gmail.com>
2017-07-22 16:13:39 +0800

    nested cv

--- ch06/grid.py
+++ ch06/grid.py
@@ -1,5 +1,5 @@
 from data import *
-
+import numpy as np
 from sklearn.grid_search import GridSearchCV
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
@@ -23,3 +23,27 @@ print(gs.best_params_)
 clf = gs.best_estimator_
 clf.fit(X_train, y_train)
 print('Test accuracy: %.3f' % clf.score(X_test, y_test))
+
+########### nested cross-validation
+gs = GridSearchCV(
+    estimator=pipe_svc, param_grid=param_grid,
+    scoring='accuracy', cv=2, n_jobs=-1
+)
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+print('SVC CV accuracy: %.3f +/- %.3f' % (
+    np.mean(scores), np.std(scores)
+))
+
+from sklearn.tree import DecisionTreeClassifier
+gs = GridSearchCV(
+    estimator=DecisionTreeClassifier(random_state=0),
+    param_grid=[
+        {'max_depth': [1,2,3,4,5,6,7,None]}
+    ],
+    scoring='accuracy', cv=5
+)
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy',cv=2)
+print('DT CV accuracy: %.3f +/- %.3f' % (
+    np.mean(scores), np.std(scores)
+))
